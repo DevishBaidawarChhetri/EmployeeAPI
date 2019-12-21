d87b003c64d52e196b4d7b55c38bd1da3de077a5
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2019-12-21 16:37:45 +0545

    Register and Search Employee Activity added

--- app/src/main/java/np/com/devish/employeeapi/DashboardActivity.java
+++ app/src/main/java/np/com/devish/employeeapi/DashboardActivity.java
@@ -2,6 +2,7 @@ package np.com.devish.employeeapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,7 +34,8 @@ public class DashboardActivity extends AppCompatActivity {
         btnRegisterEmployee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(DashboardActivity.this, RegisterEmployeeActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -41,7 +43,8 @@ public class DashboardActivity extends AppCompatActivity {
         btnSearchEmployee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(DashboardActivity.this, SearchEmployeeActivity.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/np/com/devish/employeeapi/SearchEmployeeActivity.java
@@ -0,0 +1,14 @@
+package np.com.devish.employeeapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_employee);
+    }
+}
