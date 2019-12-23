acdd3696f78929b9612cf6f490e75a9dc131962a
DevishBaidawarChhetri <devishbaidawar@gmail.com>
2019-12-23 14:25:58 +0545

    EmployeeCUD class added Employee REgisterd

--- app/src/main/java/np/com/devish/employeeapi/API/EmployeeAPI.java
+++ app/src/main/java/np/com/devish/employeeapi/API/EmployeeAPI.java
@@ -2,9 +2,12 @@ package np.com.devish.employeeapi.API;
 
 import java.util.List;
 
+import np.com.devish.employeeapi.EmployeeCUD;
 import np.com.devish.employeeapi.model.Employee;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface EmployeeAPI {
@@ -14,4 +17,9 @@ public interface EmployeeAPI {
     // Get employee on basis of EmpID
     @GET ("employee/{empID}")
     Call <Employee> getEmployeeByID(@Path("empID") int empID);
+
+    // Register User
+    @POST ("create")
+    Call<Void> registerEmployee(@Body EmployeeCUD emp);
+
 }
--- app/src/main/java/np/com/devish/employeeapi/RegisterEmployeeActivity.java
+++ app/src/main/java/np/com/devish/employeeapi/RegisterEmployeeActivity.java
@@ -7,6 +7,15 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import np.com.devish.employeeapi.API.EmployeeAPI;
+import np.com.devish.employeeapi.url.URL;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterEmployeeActivity extends AppCompatActivity {
 
@@ -39,6 +48,30 @@ public class RegisterEmployeeActivity extends AppCompatActivity {
                     etAge.setError("Please enter employee age");
                     return;
                 }
+                String name = etName.getText().toString();
+                Float salary = Float.parseFloat(etSalary.getText().toString());
+                int age = Integer.parseInt(etAge.getText().toString());
+
+                EmployeeCUD employeeCUD = new EmployeeCUD(name, salary, age);
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(URL.base_url)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                EmployeeAPI employeeAPI = retrofit.create(EmployeeAPI.class);
+                Call<Void> voidCall = employeeAPI.registerEmployee(employeeCUD);
+
+                voidCall.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        Toast.makeText(RegisterEmployeeActivity.this, "You have been successfully registered!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(RegisterEmployeeActivity.this, "Failed to register.", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
